14d8b658e9812d6f8ce7f738444f17e90091d4cf
Mateusz Drewniak <matmg24@gmail.com>
2022-09-03 00:54:33 +0200

    bump

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-require 'simplecov-cobertura'
+# require 'simplecov'
+# require 'simplecov-cobertura'
 
-::SimpleCov.start do
-  add_filter '/test/'
-  add_group 'Amber Component', 'lib/'
-end
+# ::SimpleCov.start do
+#   add_filter '/test/'
+#   add_group 'Amber Component', 'lib/'
+# end
 
-::SimpleCov.formatter = ::SimpleCov::Formatter::MultiFormatter.new([
-  ::SimpleCov::Formatter::HTMLFormatter,
-  ::SimpleCov::Formatter::CoberturaFormatter
-])
+# ::SimpleCov.formatter = ::SimpleCov::Formatter::MultiFormatter.new([
+#   ::SimpleCov::Formatter::HTMLFormatter,
+#   ::SimpleCov::Formatter::CoberturaFormatter
+# ])
 
-$LOAD_PATH.unshift ::File.expand_path('../lib', __dir__)
-require 'amber_extension_generator'
+# $LOAD_PATH.unshift ::File.expand_path('../lib', __dir__)
+# require 'amber_extension_generator'
 
-require 'byebug'
-require 'minitest/autorun'
-require 'shoulda-context'
+# require 'byebug'
+# require 'minitest/autorun'
+# require 'shoulda-context'
 
-class ::TestCase < ::Minitest::Test; end
+# class ::TestCase < ::Minitest::Test; end
